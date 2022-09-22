6b0a1222b03a2188680a96ed0eb97143fba80f46
dannywinter22 <69557137+dannywinter22@users.noreply.github.com>
2022-09-22 09:58:46 -0700

    Genshin Impact System Project
    Adding the project

---
+++ Assets/Scripts/StateMachine/StateMachine.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GenshinImpactMovementSystem
+{
+    public abstract class StateMachine
+    {
+        protected IState currentState;
+
+        public void ChangeState(IState newState)
+        {
+            currentState?.Exit();
+
+            currentState = newState;
+
+            currentState.Enter();
+        }
+        public void HandleInput()
+        {
+            currentState?.HandleInput();
+        }
+        public void Update()
+        {
+            currentState?.Update();
+        }
+        public void PhysicsUpdate()
+        {
+            currentState?.PhysicsUpdate();
+        }
+    }
+}
