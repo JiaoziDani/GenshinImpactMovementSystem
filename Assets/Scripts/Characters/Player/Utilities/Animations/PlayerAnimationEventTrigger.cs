2466631112d0ab56a6a71c7018452551c9443501
dannywinter22 <69557137+dannywinter22@users.noreply.github.com>
2022-09-29 10:14:47 -0700

    Version020.2 Complete

--- Assets/Scripts/Characters/Player/Utilities/Animations/PlayerAnimationEventTrigger.cs
+++ Assets/Scripts/Characters/Player/Utilities/Animations/PlayerAnimationEventTrigger.cs
@@ -13,18 +13,38 @@ namespace GenshinImpactMovementSystem
         }
 
         public void TriggerOnMovementStateAnimationEnterEvent()
-        {
+        {   
+            if (IsInAnimationTransition())
+            {
+                return;
+            }
+
             player.OnMovementStateAnimationEnterEvent();
         }
 
         public void TriggerOnMovementStateAnimationExitEvent()
-        {
+        {   
+            if (IsInAnimationTransition())
+            {
+                return;
+            }
+
             player.OnMovementStateAnimationExitEvent();
         }
-
+        
         public void TriggerOnMovementStateAnimationTransitionEvent()
-        {
+        {   
+            if (IsInAnimationTransition())
+            {
+                return;
+            }
+            
             player.OnMovementStateAnimationTransitionEvent();
+        }
+
+        private bool IsInAnimationTransition(int layerIndex = 0)
+        {
+            return player.Animator.IsInTransition(layerIndex);
         }
     }
 }
